cff689ea25319ab4ca36c48d32960aa22c82929c
Evan Dewitt <101440950+dewitte0210@users.noreply.github.com>
2023-10-06 17:36:56 -0500

    FIxed VBO Benchmark

--- Project2/src/ofApp.cpp
+++ Project2/src/ofApp.cpp
@@ -30,11 +30,12 @@ void ofApp::draw(){
 	cam.fov = radians(100.0f);
 	float aspect = static_cast<float>(ofGetWindowWidth()) / ofGetWindowHeight();
 	cam.pos += (velocity * speed * ofGetLastFrameTime());
+
 	// With Vbo avg fps is 11, without Vbo avg fps is 5.7
 	meshShader.begin();
-	for (int i = 0; i < 5000; i++) {
+	for (int i = 0; i < 1000; i++) {
 		mat4 model{ translate(vec3(0,0,-2)) * rotate(radians(-90.0f), vec3(1.0f,0.0f,0.0f)) * scale(vec3(0.08,0.08,0.08))};
-		mat4 view{ translate(-cam.pos) * rotate(cameraHead, vec3(0,1,0)) * rotate(cameraTilt,vec3(1,0,0)) };
+		mat4 view{ rotate(cameraHead, vec3(0,1,0)) * rotate(cameraTilt,vec3(1,0,0))* translate(-cam.pos)};
 		mat4 projection{ perspective(cam.fov, aspect, 0.01f, 10.0f) };
 		mat4 mvp{ projection * view * model };
 
@@ -43,13 +44,13 @@ void ofApp::draw(){
 	//	staff.draw();
 
 		model = translate(vec3(-1, 0, -1)) * rotate(radians(-90.0f), vec3(1.0f, 0.0f, 0.0f)) * scale(vec3(0.25, 0.25, 0.25));
-		view = translate(-cam.pos) * rotate(cameraHead, vec3(0, 1, 0)) * rotate(cameraTilt, vec3(1, 0, 0));
 		mvp = projection * view * model;
 		meshShader.setUniformMatrix4f("mvp", mvp);
 		shapesVbo.drawElements(GL_TRIANGLES, shapesVbo.getNumIndices());
 	//	shapes.draw();
-		meshShader.end();
 	}
+	meshShader.end();
+
 }
 
 //--------------------------------------------------------------
